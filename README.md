88ec359be687153f40e535c42d6adac93a5dd18b
DESKTOP-5EMPGQL\Esmanios <beoms42@gmail.com>
2022-01-13 12:55:33 +0900

    splash + socket connect

---
+++ README.md
@@ -0,0 +1,12 @@
+# DistopiaTalk
+- 소켓통신연습, 카카오톡 카피 (상업적 용도로 사용하지 않음)
+
+# KAKAO OVEN
+- https://ovenapp.io/view/deEF7YYyAVJeeOZEtvnSwcX9N6f2kmso/ - 시안제작
+
+# TISTORY
+- https://fineday-42.tistory.com/4 - 스플래시아트 제작
+- https://fineday-42.tistory.com/5 - 스플래시아트 적용
+- https://fineday-42.tistory.com/6 - 소켓통신 초급 연습
+- https://fineday-42.tistory.com/7?category=911016 - 소켓통신 안드로이드 - 이클립스 간 연동
+- https://fineday-42.tistory.com/8?category=911016 - 소켓통신 안드로이드 - 이클립스 간 채팅
---
+++ app/src/main/java/com/example/distopiatalk/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.distopiatalk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashactvity_2sec);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
